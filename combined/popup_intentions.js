30d21f3c13dc5701021c0b29a2570b39b1ed8431
TomSteenbergen <tomsteenbergen1995@gmail.com>
2020-07-25 12:53:28 +0200

    Add created timestamp to intentions

--- combined/popup_intentions.js
+++ combined/popup_intentions.js
@@ -5,7 +5,8 @@ let intentions = localStorage.getItem("intentions");
 const generateTemplate = intention => {
     const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
-      <span>${intention}</span>
+      <span>${intention.text}</span>
+      <div class="hidden_area">Created ${dateFns.distanceInWords(new Date(), intention.createTimestamp)} ago</div>
       <i class="far fa-trash-alt delete"></i>
     </li>
   `;
@@ -23,7 +24,7 @@ const fullAddFormTemplate = "<form class=\"add text-center my-4\">\n" +
 
 // If there are no intentions yet, input a default intentions.
 if (!intentions) {
-    intentions = ["Create your first intention!"];
+    intentions = [{text: "Create your first intention!", createTimestamp: new Date()}];
     localStorage.setItem("intentions", JSON.stringify(intentions));
 } else {
     intentions = JSON.parse(intentions);
@@ -37,15 +38,15 @@ intentions.forEach(intention => {
 // Add a listener for adding new intentions.
 addForm.addEventListener('submit', e => {
     e.preventDefault();
-    const newIntentions = addForm.add.value.trim();
+    const newIntentions = {text: addForm.add.value.trim(), createTimestamp: new Date()};
     let intentions = JSON.parse(localStorage.getItem("intentions"));
     // Only allow a maximum of 3 intentions.
-    if (newIntentions.length && intentions.length < 3) {
+    if (newIntentions.text.length && intentions.length < 3) {
         intentions.push(newIntentions);
         localStorage.setItem("intentions", JSON.stringify(intentions));
         generateTemplate(newIntentions);
         addForm.reset();
-    } else if (newIntentions.length) {
+    } else if (newIntentions.text.length) {
         // Update HTML to show a warning in case a fourth intention is entered.
         addForm.innerHTML = fullAddFormTemplate
     }
@@ -55,12 +56,12 @@ addForm.addEventListener('submit', e => {
 list.addEventListener('click', e => {
     if (e.target.classList.contains('delete')) {
         let intentions = JSON.parse(localStorage.getItem("intentions"));
-        intentions = intentions.filter(item => item !== e.target.parentElement.innerText);
+        intentions = intentions.filter(item => !e.target.parentElement.innerHTML.includes(`<span>${item.text}</span>`));
         localStorage.setItem("intentions", JSON.stringify(intentions));
         e.target.parentElement.remove();
     }
     // Update the HTML if there is room to add more intentions.
-    if (JSON.parse(localStorage.getItem("intentions")).length <= 2) {
+    if (JSON.parse(localStorage.getItem("intentions")).text.length <= 2) {
         addForm.innerHTML = addFormTemplate
     }
 });
